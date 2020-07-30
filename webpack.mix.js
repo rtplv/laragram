7ef5d6f34784502b692d2cdfbaf255206a2832bd
Ruslan Tupolev <rtplv@yandex.ru>
2020-07-30 12:17:21 +0300

    Remove generated files from public

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,4 +22,5 @@ mix.webpackConfig({
 mix.browserSync('laragram.ru');
 
 mix.js('resources/js/layouts/main/index.js', 'public/js/layouts/main.js')
-    .sass('resources/sass/layouts/main/index.scss', 'public/css/layouts/main.css');
+    .sass('resources/sass/layouts/main/index.scss', 'public/css/layouts/main.css')
+    .copy('resources/assets', 'public/assets');
