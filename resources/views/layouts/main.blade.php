8923dacd482421bbbc865d8b0e4135525bc275b9
Ruslan Tupolev <rtplv@yandex.ru>
2020-07-30 13:01:57 +0300

    Header styles

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,11 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+{{--    Favicon--}}
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("favicon/apple-touch-icon.png") }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("favicon/favicon-32x32.png") }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("favicon/favicon-16x16.png") }}">
     <link rel="manifest" href="{{ asset("favicon/site.webmanifest") }}">
 
+{{--    Fonts--}}
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+
+{{--    Styles--}}
     <link rel="stylesheet" href="{{ mix('css/layouts/main.css') }}">
 
     <title>Laragram</title>
@@ -24,6 +30,7 @@
 
 
 {{--    @include('layouts.parts.main.footer')--}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js" integrity="sha512-YSdqvJoZr83hj76AIVdOcvLWYMWzy6sJyIMic2aQz5kh2bPTd9dzY3NtdeEAzPp/PhgZqr4aJObB3ym/vsItMg==" crossorigin="anonymous"></script>
     <script src="{{ mix('js/layouts/main.js') }}"></script>
 </body>
 </html>
--- resources/views/layouts/parts/main/header.blade.php
+++ resources/views/layouts/parts/main/header.blade.php
@@ -1,10 +1,14 @@
 <header class="main-layout-header">
-    <article class="main-layout-header__logo">
-        laragram
+    <article class="main-layout-header-logo">
+        <a href="/">
+            <img class="main-layout-header-logo__image"
+                 src="{{ asset('assets/logo.png') }}"
+                 alt="Laragram">
+        </a>
     </article>
 
-    <nav class="main-layout-header__navigation">
-        <ul>
+    <nav class="main-layout-header-navigation">
+        <ul class="ui-navbar">
             <li>
                 <a href="#">
                     Лента
@@ -13,7 +17,11 @@
         </ul>
     </nav>
 
-    <article class="main-layout-header__user-block">
-        dropdown
+    <article class="main-layout-header-user">
+        <a class="main-layout-header-user__link ui-link"
+           href="#">
+            Ruslan Tupolev
+            <i class="main-layout-header-user__link-icon fas fa-chevron-down"></i>
+        </a>
     </article>
 </header>
