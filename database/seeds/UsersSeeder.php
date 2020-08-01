46aa45bd1f62cc5cc869ca66baaa921aa799bb2c
Ruslan Tupolev <rtplv@yandex.ru>
2020-08-01 13:44:34 +0300

    Add users seeder

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::query()->create([
+            'name' => 'demo',
+            'email' => 'demo@demo.ru',
+            'password' => 'demo'
+        ]);
+    }
+}
